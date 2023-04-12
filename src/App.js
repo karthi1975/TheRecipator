8102cddda49d61774774fab0b8e8a14b0c5fe9d7
karthi1975 <karthi.jeyabalan@gmail.com>
2023-04-11 21:50:50 -0600

    Updated Julios code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 import { Tabs, Tab, Panel } from '@bumaga/tabs';
 import ratatouille from './ratatouille2.png';
 // import Rtabs from './Rtabs';
-//import Dtime from './Dtime';
+// <<<<<<< HEAD
+// import Dtime from './Dtime';
+// import Ingsubs from './Ingsubs';
+// =======
+// //import Dtime from './Dtime';
+// >>>>>>> 93e68ac36164eda349dc62911f35f0d472437e0f
 
 
 function App() {
@@ -16,14 +21,13 @@ function App() {
   const [recipes, setRecipes] = useState([]);
   // const [ subsIngredient, setSubsIngredient] = useState('');
 
-
-  const handleSubmit = async (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Make an HTTP request to the Flask API with the ingredients
     const response = await axios.post(
-        'http://34.28.150.158:5001/recommend',
-        { include: ingredients, exclude: removedIngredients.join(',') }
+      'http://localhost:5001/recommend',
+      { include: addedIngredients.join(' '), exclude: removedIngredients.join(' ') }
     );
 
     // Update the state with the recommended recipes
@@ -34,21 +38,44 @@ function App() {
     console.log('Recommended recipes:', recommendedRecipes);
     setRecipes(recommendedRecipes);
     console.log('Recipes state:', recipes);
+
+    // Clear the addedIngredients and removedIngredients arrays
+    // setAddedIngredients([]);
+    // setRemovedIngredients([]);
   };
 
+const handleClear = () => {
+    setAddedIngredients([]);
+    setRemovedIngredients([]);
+    setRecipes([]);
+};
 
-  const handleAdd = () => {
-    // Add a new ingredient to the list of ingredients
-    const newIngredient = prompt('Enter ingredient to INCLUDE in the recipe:');
-    setIngredients(ingredients + ' ' + newIngredient);
-    setAddedIngredients([...addedIngredients, newIngredient]);
-  };
-  const handleRemove = () => {
-    // Remove an ingredient from the list of ingredients
-    const ingredientToRemove = prompt('Enter ingredient to EXCLUDE from the recipe:');
-    setIngredients(ingredients.replace(ingredientToRemove, ''));
-    setRemovedIngredients([...removedIngredients, ingredientToRemove]);
-  };
+const handleAdd = () => {
+  // Add new ingredients to the list of ingredients
+  const newIngredients = ingredients.split(' ').filter((ingredient) => {
+    return ingredient.trim() !== '' && !addedIngredients.includes(ingredient);
+  });
+  setAddedIngredients([...addedIngredients, ...newIngredients]);
+  setRemovedIngredients(removedIngredients.filter((ingredient) => !newIngredients.includes(ingredient)));
+  setIngredients('');
+};
+
+const handleRemove = () => {
+  // Remove ingredients from the list of ingredients
+  const newRemovedIngredients = ingredients.split(' ').filter((ingredient) => {
+    return ingredient.trim() !== '' && !removedIngredients.includes(ingredient);
+  });
+  setRemovedIngredients([...removedIngredients, ...newRemovedIngredients]);
+  setAddedIngredients(addedIngredients.filter((ingredient) => !newRemovedIngredients.includes(ingredient)));
+  setIngredients('');
+};
+
+<Form.Control
+  type="text"
+  placeholder="oil garlic fish"
+  value={ingredients}
+  onChange={(e) => setIngredients(e.target.value)}
+/>
 
 
   // const handleSubstitute = async (e) => {
@@ -69,19 +96,25 @@ function App() {
     <div className="container">
 
       <div className="form-container">
+
+
         {/*<h4>⏳ Preparation time h:mm</h4><Dtime />*/}
+
 
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="formIngredients">
-            <h4>Ingredients</h4>
+            {/*<h4>Ingredients</h4>*/}
+            <h4>Include / Exclude ingredients then Submit</h4>
             <Form.Label></Form.Label>
             <Form.Control type="text" placeholder="oil garlic fish" value={ingredients} onChange={(e) => setIngredients(e.target.value)} />
+            <Button variant="primary" type="submit" className="w-100">
+              Submit
+            </Button>
+            <Button variant="danger" onClick={handleClear} className="clear-button">
+              Clear
+            </Button>
           </Form.Group>
-          <Button variant="primary" type="submit" className="w-100">
-            Submit
-          </Button>
         </Form>
-
         <div>
           <Button
               variant="secondary"
@@ -91,7 +124,6 @@ function App() {
           </Button>{' '}
           <Button variant="secondary" onClick={handleRemove} className="remove-button">Exclude</Button>
         </div>
-
         <div>
           {addedIngredients.length > 0 && (
             <div>
@@ -122,10 +154,14 @@ function App() {
         <div className={recipes.length > 0 ? "recipes-container" : "hidden"}>
           {recipes.map((recipe, index) => (
             <div key={index} className="recipe-content">
-              {recipe.content.title}
+                  {recipe.content.title}
             </div>
-          ))}
+          ))
+          }
         </div>
+
+        <h4>Ingredient Substitutions</h4>
+        {/*<Ingsubs/>*/}
 
       </div>
 
@@ -145,12 +181,13 @@ function App() {
             <Panel key={index}>
               <h3>{recipe.content.title}</h3>
               <p>{recipe.content.description}</p>
-              <img src = {recipe.content.image=='https://images.media-allrecipes.com/images/79590.png' ? ratatouille : recipe.content.image}
+              <img src = {recipe.content.image === 'https://images.media-allrecipes.com/images/79590.png' ? ratatouille : recipe.content.image}
                    alt="display image"
                    width={300}/>
+              <p><b>Prep time:</b>   h:mm   <b>Ready time:</b>  h:mm  </p>
               <h4>Ingredients</h4>
               <ul>
-                {recipe.content.ingredients.substring(2, recipe.content.ingredients.length -2).split("\n").map((value, index ) => (
+                {recipe.content.ingredients.substring(1, recipe.content.ingredients.length -1).split("\n").map((value, index ) => (
                   <li key={index}>
                     {value}
                     <br />
